54abe7d82212ed290134fb4e7bfbb6bedf8a95e3
nolann7 <nolann709@gmail.com>
2022-10-11 13:17:40 +0300

    implementing useReducer

--- src/components/Meals/Meal/MealItemForm.jsx
+++ src/components/Meals/Meal/MealItemForm.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
+  
   return (
     <form className={classes.form} >
       <Input
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultState = {
+  items: [],
+  totalAmount: 0,
+};
+function cartReducer(state, action) {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  if (action.type === 'REMOVE') {
+    
+  }
+  return defaultState;
+}
+
 const CartProvider = props => {
-  const addItemHandler = item => {};
-  const removeItemHandler = id => {};
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultState);
+
+  const addItemHandler = item => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+  const removeItemHandler = id => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
     items: [],
