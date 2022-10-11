730b53dce0d92b8d65c0787a23fdac08ec1249a1
nolann7 <nolann709@gmail.com>
2022-10-11 13:20:23 +0300

    implementing useRef on custom Input

--- src/components/Meals/Meal/MealItemForm.jsx
+++ src/components/Meals/Meal/MealItemForm.jsx
@@ -1,12 +1,14 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
-  
+  const amountInputRef = useRef();
+
   return (
-    <form className={classes.form} >
+    <form className={classes.form}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount' + props.id,
@@ -17,7 +19,7 @@ const MealItemForm = props => {
           defaultValue: '1',
         }}
       />
-      <button type='submit'>+ Add</button>
+      <button type="submit">+ Add</button>
     </form>
   );
 };
